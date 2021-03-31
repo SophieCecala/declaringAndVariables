f393dfa4f7292cbc8c361734654c5639a5e9b764
Natalya Myunster <natalyamyunster@yahoo.com>
2021-03-31 22:41:26 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const name ='Nikita';
+let age = 12;
+age = 13;
+alert(name + ' is ' + age + ' years old.');
+
+let a = 1;
+let b = a + 3;
+let c = a + b;
+alert(c);
+
+
+let cups = 4;
+let sauc = 4;
+let spoons = 4;
+alert(cups + sauc + spoons);
